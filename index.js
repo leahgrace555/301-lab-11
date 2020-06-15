e43323733b2bf1c08802657fe4aefe335df0e0a5
Leah <leah.grace555@gmail.com>
2020-06-15 19:07:27 -0400

    test route working

--- index.js
+++ index.js
@@ -3,11 +3,22 @@
 const express = require('express');
 const app = express();
 
+require('ejs');
+app.set('view engine','ejs');
+
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./Public'));
+
+//test route:
+app.get('/hello', (request,response) => {
+  response.render('pages/index.ejs');
+})
 
 
