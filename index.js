c590486a6382071e440ac48b4f0c5f8b04c29b12
AndrewCS149 <andrewcs149@gmail.com>
2020-06-15 21:04:08 -0700

    Create error view that will display if an error occurs on searches.

--- index.js
+++ index.js
@@ -3,15 +3,12 @@
 const express = require('express');
 const app = express();
 const superagent = require('superagent');
+// const { response } = require('express');
 require('ejs');
 app.set('view engine','ejs');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./Public'));
@@ -21,14 +18,15 @@ app.get('/hello', (request,response) => {
   response.render('pages/index.ejs');
 })
 
+
 //searches route
 app.get('/searches/new', (request,response) => {
   response.render('pages/searches/new.ejs')
 })
 
+// searches route
 app.post('/searches',(request,response) => {
   let url = 'https://www.googleapis.com/books/v1/volumes?q='
-
 
   let query = request.body.search[0];
   let titleOrAuthor = request.body.search[1];
@@ -47,14 +45,19 @@ app.post('/searches',(request,response) => {
       });
 
       response.render('pages/searches/show.ejs', {searchResults: books});
-    }).catch(err => error(err, response));
+    }).catch(response.render('pages/error.ejs'));
 });
+
+// error route
+// app.get('*', (request, response) => {
+//   response.render('pages/error.ejs');
+// })
 
 //book construction
 function Book(info) {
   this.title = info.volumeInfo.title ? info.volumeInfo.title: 'not available';
   this.author = info.volumeInfo.authors;
-  this.description = info.volumeInfo.description;
+  this.description = info.volumeInfo.description
 
   let img = info.volumeInfo.imageLinks.smallThumbnail;
   let reg = /^https/;
@@ -68,10 +71,14 @@ function Book(info) {
   }
 }
 
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+
 // 500 error message
 const error = (err, res) => {
   console.log('Error', err);
   res.status(500).send('There was an error on our part.');
 }
 
-
+// const placeholderImg = 'https://i.imgur.com/J5LVHEL.jpg';
